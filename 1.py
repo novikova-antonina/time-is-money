cea4a6559940d9944127b57f6631444e2a881e56
Antonina <novikovantoninalex@gmail.com>
2025-04-23 11:52:40 +0400

    Add new repo

---
+++ 1.py
@@ -0,0 +1,9 @@
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+print(render_progressbar(10, 5))
--- time_1.py
+++ time_1.py
@@ -1,10 +1,11 @@
 import os
-from pytimeparse import parse
 
 from dotenv import load_dotenv
+from pytimeparse import parse
 import ptbot
 
 load_dotenv()
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
@@ -12,39 +13,51 @@ bot = ptbot.Bot(TELEGRAM_TOKEN)
 
 
 def reply(chat_id, text):
-    seconds = parse(text)
-
-    message_id = bot.send_message(chat_id, create_progress_message(seconds, seconds))
-
-    def on_tick(remaining_time):
-        bot.update_message(
-            chat_id, message_id, create_progress_message(seconds, remaining_time)
+    input_seconds = parse(text)
+    if not input_seconds:
+        bot.send_message(
+            chat_id, "Пожалуйста, укажите корректное время (например, '1m30s')."
         )
+        return
 
-        if remaining_time == 0:
-            on_finish()
+    message_id = bot.send_message(chat_id, "Таймер запущен")
+    bot.create_countdown(
+        input_seconds,
+        notify,
+        chat_id=chat_id,
+        message_id=message_id,
+        total_seconds=input_seconds,
+    )
 
-    def on_finish():
+
+def notify(secs_left, chat_id, message_id, total_seconds):
+    remaining_time = format_time(secs_left)
+    progress_bar = render_progressbar(total_seconds, total_seconds - secs_left)
+    bot.update_message(
+        chat_id, message_id, f"Осталось времени: {remaining_time}\n{progress_bar}"
+    )
+    if secs_left == 0:
         bot.send_message(chat_id, "Время вышло!")
 
-    bot.create_countdown(seconds, on_tick)
 
-
-def create_progress_message(total_seconds, remaining_seconds):
-    progress_bar = render_progressbar(total_seconds, remaining_seconds)
-    return f"Осталось секунд: {remaining_seconds}\n{progress_bar}"
+def format_time(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return f"{minutes:02}:{remaining_seconds:02}"
 
 
 def render_progressbar(
     total, iteration, prefix="", suffix="", length=30, fill="█", zfill="░"
 ):
-    percent = f"{100 * (iteration / float(total)):.1f}"
+    iteration = min(total, iteration)
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return f"{prefix} |{pbar}| {percent}% {suffix}"
+    return "{0} |{1}| {2}% {3}".format(prefix, pbar, percent, suffix)
 
 
 def main():
+
     bot.reply_on_message(reply)
     bot.run_bot()
 
